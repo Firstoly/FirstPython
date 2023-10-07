22af4857d99bfdfa4786404a3acf291e06f8d2ae
Firstoly <139094262+Firstoly@users.noreply.github.com>
2023-10-07 14:09:59 +0700

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+name = input('Enter name :')
+surname = input('Enter surname : ')
+age = int(input('Enter age : '))
+vlue = age+4
+print('\n')
+print ('My name is ',name)
+print ('My surname is ',surname)
+print ('Now I\'m',age,'years old.')
+print ('I\'ii finish undergraduste in',vlue,'years old.')
